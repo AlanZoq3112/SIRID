432a1b4d09ca50e3099c208d3e9f875bd3dd0e62
Noé Mérida <20213tn121@utez.edu.mx>
2023-02-16 16:23:05 -0600

    inicio del proyecto

---
+++ src/main/java/mx/edu/utez/sirid/SiridApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sirid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/sirid/SiridApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.sirid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
