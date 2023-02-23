2181c799fe3962d6d61c7dcd7cf04afbf7fd917a
AlixStart313 <20213tn014@utez.edu.mx>
2023-02-22 22:20:29 -0600

    controller (?)

--- src/main/java/mx/edu/utez/sirid/controller/Area/AreaController.java
+++ src/main/java/mx/edu/utez/sirid/controller/Area/AreaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api-sirid/area")
+@RequestMapping("/api-sirid/area/")
 @CrossOrigin(origins = {"*"})
 public class AreaController {
     @Autowired
