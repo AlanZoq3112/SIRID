fa820c770e4bee8a5d247ca938b077d47a5125e1
LuisKevL <20213tn118@utez.edu.mx>
2023-02-23 00:01:25 -0600

    StatusFuncionando

--- src/main/java/mx/edu/utez/sirid/controller/Status/StatusController.java
+++ src/main/java/mx/edu/utez/sirid/controller/Status/StatusController.java
@@ -1,4 +1,54 @@
 package mx.edu.utez.sirid.controller.Status;
+import mx.edu.utez.sirid.controller.Status.dtos.StatusDTO;
+import mx.edu.utez.sirid.model.Status.Status;
+import mx.edu.utez.sirid.service.Status.StatusService;
+import mx.edu.utez.sirid.utils.CustomResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api-sirid/status/")
+@CrossOrigin(origins = {"*"})
 public class StatusController {
+    @Autowired
+    private StatusService service;
+
+    @GetMapping("/")
+    public ResponseEntity<CustomResponse<List<Status>>> getAll() {
+        return new ResponseEntity<>(
+                this.service.getAll(),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomResponse<Status>> getOne(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(
+                this.service.getOne(id),
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<CustomResponse<Status>> insert(
+            @RequestBody StatusDTO statusDTO, @Valid BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(
+                    null,
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+        return new ResponseEntity<>(
+                this.service.insert(statusDTO.getStatus()),
+                HttpStatus.CREATED
+        );
+    }
+
+
 }
--- src/main/java/mx/edu/utez/sirid/controller/Status/dtos/StatusDTO.java
+++ src/main/java/mx/edu/utez/sirid/controller/Status/dtos/StatusDTO.java
@@ -1,4 +1,29 @@
 package mx.edu.utez.sirid.controller.Status.dtos;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mx.edu.utez.sirid.model.Status.Status;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class StatusDTO {
+    private long id;
+
+    @NotEmpty(message = "Campo Obligatorio")
+    @Size(min = 1, max = 100)
+    private String name;
+
+    public Status getStatus(){
+        return new Status(
+                getId(),
+                getName()
+        );
+    }
+
 }
