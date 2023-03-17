25044b2261654330f7d1c1f0ade15b47daf8b504
AlixStart313 <20213tn014@utez.edu.mx>
2023-03-17 00:17:36 -0600

    comienzo de las apis FALTANTES

--- src/main/java/mx/edu/utez/sirid/model/User/IUserRepository.java
+++ src/main/java/mx/edu/utez/sirid/model/User/IUserRepository.java
@@ -1,6 +1,7 @@
 package mx.edu.utez.sirid.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -8,11 +9,11 @@ import java.util.List;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
     boolean findById(long id);
-
     User findByCorreoElectronico(String email);
-
     List<User> findAll();
+    Boolean existsById(String user);
 
+    @Modifying
     @Query(value = "UPDATE User set status =: status WHERE ID =: id",
             nativeQuery = true
     )
@@ -21,6 +22,42 @@ public interface IUserRepository extends JpaRepository<User, Long> {
             @Param("id") Long id
     );
 
-    Boolean existsById(String user);
+    //query para solicitar la recuperacion de contraseña (contraseña olvidada"
+    @Modifying
+    @Query(value = "UPDATE User set contrasena =:newContrasena,changeStatus=:0 WHERE id =: id",
+        nativeQuery = true
+    )
+    Boolean recoverPassword(
+            @Param("newContrasena") String newContrasena,
+            @Param("id") Long id
+    );
+
+    //query para cambiar la contraseña
+    @Modifying
+    @Query(value = "UPDATE User set contrasena =:newContrasena WHERE id =: id",
+            nativeQuery = true
+    )
+    Boolean changePassword(
+            @Param("newContrasena") String newContrasena
+    );
+
+   //Query para ver a todos los profesores
+   @Modifying
+    @Query(value = "SELECT * from USERS WHERE role_id =: 3",
+           nativeQuery = true
+   )
+   List<User> lookAllTeachers();
+
+    //Query para ver a todos el personal de soporte
+    @Modifying
+    @Query(value = "SELECT * from USERS WHERE role_id =: 2",
+            nativeQuery = true
+    )
+    List<User> lookAllSupportTeam();
+
+
+
+
+
 }
 
